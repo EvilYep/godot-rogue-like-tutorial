b8c36a265fa28b5e47f209583c8f77055eadcf48
EvilYep <guillaume.moneret@gmail.com>
2023-02-21 06:59:42 +0100

    animated sprite and better conditionals :joystick:

--- Scenes/Character/Player.gd
+++ Scenes/Character/Player.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+onready var animated_sprite = $AnimatedSprite
+
 var speed : float = 300.0
 var direction = Vector2.ZERO
 
@@ -10,6 +12,16 @@ func _process(delta: float) -> void:
 	move_and_slide(direction * speed)
 
 func _input(event: InputEvent) -> void:
-	direction.x = int(event.is_action_pressed("ui_right")) - int(event.is_action_pressed("ui_left"))
-	direction.y = int(event.is_action_pressed("ui_down")) - int(event.is_action_pressed("ui_up"))
+	direction.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	direction.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	
+	direction = direction.normalized()
 
+	match(direction):
+		Vector2.RIGHT: animated_sprite.play("MoveRight")
+		Vector2.LEFT: animated_sprite.play("MoveLeft")
+		Vector2.DOWN: animated_sprite.play("MoveDown")
+		Vector2.UP: animated_sprite.play("MoveUp")
+		Vector2.ZERO:
+			animated_sprite.stop()
+			animated_sprite.set_frame(0)
